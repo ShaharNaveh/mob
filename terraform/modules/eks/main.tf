283227135a4cd9b88367ff8ea267a7d186b3cde4
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2022-10-29 17:34:52 +0300

    Allow access CP -> webhook of AWS ALB controller

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -11,6 +11,17 @@ module "eks" {
   subnet_ids                     = var.subnet_ids
   cluster_endpoint_public_access = var.cluster_endpoint_public_access
 
+  node_security_group_additional_rules = {
+    ingress_allow_access_from_control_plane = {
+      type                          = "ingress"
+      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+      protocol                      = "tcp"
+      from_port                     = 9443
+      to_port                       = 9443
+      source_cluster_security_group = true
+    }
+  }
+
   eks_managed_node_group_defaults = {
     disk_size      = 20
     instance_types = ["t2.small"]
